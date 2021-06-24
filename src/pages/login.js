e0734524f75bce09c99811733286df4b820b8e3d
Joe Snyder <snyderjosephd@yahoo.com>
2021-06-24 08:29:00 -0500

    More work on login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useState, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
   const history = useHistory();
@@ -12,7 +13,18 @@ export default function Login() {
 
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress('');
+      setPassword('');
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = 'Login - Instagram';
@@ -24,42 +36,46 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="iPhone" />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
-          <input
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <button
-            disabled={isInvalid}
-            type="submit"
-            className={`bg-blue-500 text-white w-full rounded h-8 font-bold
-            ${isInvalid && 'opacity-50'}`}
-          >
-            Log In
-          </button>
-        </form>
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+              ${isInvalid && 'opacity-50'}`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
+          <p className="text-sm">
+            Don't have an account?{' '}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign Up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
-// TODO: add to tailwind config
-// bg-blue-medium -> hex values
-// text-red-primary -> hex values
-// text-gray-base -> hex values
-// border-gray-primary -> hex values
