cbedfcd45da798d4fe1216867585478a40bcbc37
Joe Snyder <snyderjosephd@yahoo.com>
2021-07-06 08:32:01 -0500

    Building out sidebar components

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
-import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 import UserContext from '../context/user';
+import { getUserByUserId } from '../services/firebase';
 
 export default function useUser() {
   const [activeUser, setActiveUser] = useState({});
@@ -10,7 +10,7 @@ export default function useUser() {
     async function getUserObjByUserId() {
       // we need a function that we can call (firebase service) that gets the user
       // data based on the id
-
+      const [response] = await getUserByUserId(user.uid);
       setActiveUser(response);
     }
 
@@ -18,4 +18,6 @@ export default function useUser() {
       getUserObjByUserId();
     }
   }, [user]);
+
+  return { user: activeUser };
 }
