7ef48a75a5aebf686c2ec967919da7a2c2d5efad
Joe Snyder <snyderjosephd@yahoo.com>
2021-07-15 08:29:03 -0500

    Working on actions and comments components of a Post

---
+++ src/components/post/comments.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { formatDistance } from 'date-fns';
+import { Link } from 'react-router-dom';
+
+export default function Comments({ docId, comments: allComments, posted, commentInput }) {
+  const [comments, setComments] = useState(allComments);
+
+  return (
+    <>
+      <div className="p-4 pt-1">
+        {comments.length >= 1 && (
+          <p className="text-sm text-gray-base mb-1 cursor-pointer">
+            View all {comments.length} comments
+          </p>
+        )}
+      </div>
+    </>
+  );
+}
+
+Comments.propTypes = {
+  docId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
+  posted: PropTypes.number.isRequired,
+  commentInput: PropTypes.object.isRequired
+};
