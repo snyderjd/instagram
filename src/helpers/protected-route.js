e52f38bb93b3bdc088f7bc6c2eff694635f7c561
Joe Snyder <snyderjosephd@yahoo.com>
2021-07-16 08:25:28 -0500

    Can view and add comments on a post

---
+++ src/helpers/protected-route.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+
+export default function ProtectedRoutes({ user, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location}) = {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect to={{pathname: ROUTES.LOGIN }} />
+          )
+        }
+      }}
+
+    />
+  )
+}
