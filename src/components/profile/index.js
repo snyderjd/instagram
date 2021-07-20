cfdd656c9aa785983188b887d48f90ea1e315bce
Joe Snyder <snyderjosephd@yahoo.com>
2021-07-20 08:30:38 -0500

    Start working on user profile

---
+++ src/components/profile/index.js
@@ -0,0 +1,5 @@
+import { useReducer, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+
+export default function Profile({ username }) {}
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,39 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
+import Header from '../components/header';
+import UserProfile from '../components/profile';
+
 export default function Profile() {
-  return null;
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const [userExists, setUserExists] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const user = await getUserByUsername(username);
+
+      if (user.length > 0) {
+        setUser(user[0]);
+        setUserExists(true);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExists();
+    console.log(user, 'user');
+  }, [username, history]);
+  console.log('username', username);
+
+  return userExists ? (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <UserProfile />
+      </div>
+    </div>
+  ) : null;
 }
